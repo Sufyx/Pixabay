8f5acac1637724b3618069243d141bd1f21440ac
Asaf G <asafg86@gmail.com>
2023-06-28 19:34:12 +0300

    Basic search and fetch

--- main.js
+++ main.js
@@ -4,5 +4,19 @@
  * 28/06/2023
  */
 
+
 const API_KEY = '37958354-de46dabfdd71801db390aedd0';
 
+
+document.getElementById('imageSearchClick').addEventListener('click', imageSearchClick);
+function imageSearchClick(e) {
+    e.preventDefault();
+    const searchText = document.getElementById("imageSearchBox").value;
+    // console.log("search text: ", searchText);
+    const fetchString = `https://pixabay.com/api/?key=${API_KEY}&q=${searchText}&pretty=true`;
+    fetch(fetchString)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    });
+}
